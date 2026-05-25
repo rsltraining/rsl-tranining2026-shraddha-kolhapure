8795123179f0a61274d98416b072c0d1f89c7b68
Shraddha Kolhapure <shraddha.kolhapure@rajasoftwarelabs.com>
2026-05-25 12:00:43 +0530

    SceneDelegate.m

--- ViewControllerAssignment/ViewControllerAssignment/SceneDelegate.m
+++ ViewControllerAssignment/ViewControllerAssignment/SceneDelegate.m
@@ -5,14 +5,12 @@
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
     
-    // 1. Capture the system window scene
+   
     UIWindowScene *windowScene = (UIWindowScene *)scene;
     self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
     
-    // 2. Initialize your primary programmatic screen
     FirstViewController *rootVC = [[FirstViewController alloc] init];
     
-    // 3. Mount it onto the root window and display it
     self.window.rootViewController = rootVC;
     [self.window makeKeyAndVisible];
 }
