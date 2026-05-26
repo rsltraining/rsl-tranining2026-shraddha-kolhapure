636d990000b0aa1129ea8f154ab40bdeac823ced
Shraddha Kolhapure <shraddha.kolhapure@rajasoftwarelabs.com>
2026-05-26 17:02:54 +0530

    Commit : Delegation Assignment

---
+++ Delegation_DesignPattern/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Delegation_DesignPattern
+//
+//  Created by Shraddha Kolhapure on 26/05/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Delegation_DesignPattern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delegation_DesignPattern
+//
+//  Created by Shraddha Kolhapure on 26/05/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
