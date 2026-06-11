2b1d359b2a937efda86d6fc83d18d6748e8dc493
Shraddha Kolhapure <shraddha.kolhapure@rajasoftwarelabs.com>
2026-06-11 10:58:41 +0530

    Updated SecondPage.java

--- app/src/main/java/com/example/view_part2_assignment/SecondPage.java
+++ app/src/main/java/com/example/view_part2_assignment/SecondPage.java
@@ -1,16 +1,34 @@
 package com.example.view_part2_assignment;
 
 import android.os.Bundle;
-import android.widget.Button;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondPage extends AppCompatActivity {
+    private boolean isHidden = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondpage);
-
+        ImageView imgInstitute = findViewById(R.id.imgInstitute);
+        ImageView img2 = findViewById(R.id.img2);
+        TextView name2 = findViewById(R.id.name2);
+        imgInstitute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isHidden) {
+                    img2.setVisibility(View.GONE);
+                    name2.setVisibility(View.GONE);
+                    isHidden = true;
+                } else {
+                    img2.setVisibility(View.VISIBLE);
+                    name2.setVisibility(View.VISIBLE);
+                    isHidden = false;
+                }
+            }
+        });
     }
 }
